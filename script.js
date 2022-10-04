3c05ffece6f4273fd1bc33839257bbb5a3c519d5
maged21 <majedabdulaziz72@yahoo.com>
2022-10-04 21:54:33 +0200

    updates

--- script.js
+++ script.js
@@ -23,3 +23,9 @@
   function closemenu(){
     sidemenu.style.right = "-200px";
   }
+
+  const nav = document.querySelector("nav");
+
+  window.addEventListener ("scroll", function() {
+    nav.classList.toggle ("sticky", window.scrollY > 0);
+  }) ;
